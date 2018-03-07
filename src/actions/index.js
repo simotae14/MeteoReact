88d4bd726d6652c98b80440dd141ef911cda9904
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-07 13:41:59 +0100

    Request Ajax con Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 const API_KEY = 'e4ce5e259c3b4f09931b2181616b1d29';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
