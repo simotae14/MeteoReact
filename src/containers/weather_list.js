f3ade072b02545b6826ca00dbc05f6d3088d6592
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-09 11:05:20 +0100

    Google Maps Integration

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
-    //const temps = cityData.list.map(weather => weather.main.temp);
-    //const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-      <td>{name}</td>
-      <td>
-        <Sparklines height={120} width={180} data={temps}>
-          <SparklinesLine color="red" />
-        </Sparklines>
-      </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="C" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
@@ -26,22 +25,14 @@ class WeatherList extends Component {
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>
-              Città
-            </th>
-            <th>
-              Temperatura
-            </th>
-            <th>
-              Pressione
-            </th>
-            <th>
-              Umidità
-            </th>
+            <th>Città</th>
+            <th>Temperatura (C)</th>
+            <th>Pressione (hPa)</th>
+            <th>Umidità (%)</th>
           </tr>
         </thead>
         <tbody>
-          { this.props.weather.map(this.renderWeather) }
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
